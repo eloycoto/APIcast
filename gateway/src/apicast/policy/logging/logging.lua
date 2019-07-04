b048babe695af995a10a0b12fab9ea1acd03c621
Eloy Coto <eloy.coto@gmail.com>
2019-07-04 17:08:46 +0200

    new
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

--- gateway/src/apicast/policy/logging/logging.lua
+++ gateway/src/apicast/policy/logging/logging.lua
@@ -1,13 +1,20 @@
 --- Logging policy
 
 local _M  = require('apicast.policy').new('Logging Policy')
-
 local new = _M.new
 
-local default_enable_access_logs = true
+local Condition = require('apicast.conditions.condition')
+local LinkedList = require('apicast.linked_list')
+local Operation = require('apicast.conditions.operation')
+local TemplateString = require('apicast.template_string')
 
 -- Defined in ngx.conf.liquid and used in the 'access_logs' directive.
 local ngx_var_access_logs_enabled = 'access_logs_enabled'
+local ngx_var_extended_access_logs_enabled = 'extended_access_logs_enabled'
+local ngx_var_extended_access_log = 'extended_access_log'
+
+local default_enable_access_logs = true
+local default_template_type = 'plain'
 
 -- Returns the value for the ngx var above from a boolean that indicates
 -- whether access logs are enabled or not.
@@ -29,12 +36,57 @@ function _M.new(config)
   end
 
   self.enable_access_logs_val = val_for_ngx_var[enable_access_logs]
+  self.custom_logging = config.custom_logging
+  if config.condition then
+    ngx.log(ngx.DEBUG, 'Enabling extended log with conditions')
+    local operations = {}
+    for _, operation in ipairs(config.condition.operations) do
+      table.insert( operations,
+        Operation.new(
+          operation.match, operation.match_type,
+          operation.op,
+          operation.value, operation.value_type or default_template_type))
+    end
+    self.condition = Condition.new( operations, config.condition.combine_op)
+  end
 
   return self
 end
 
-function _M:log()
+function get_request_context(context)
+  local ctx = { }
+  ctx.req = {
+    headers=ngx.req.get_headers(),
+  }
+
+  ctx.resp = {
+    headers=ngx.resp.get_headers(),
+  }
+
+  ctx.service = context.service or {}
+  return LinkedList.readonly(ctx, ngx.var)
+end
+
+function _M:log(context)
   ngx.var[ngx_var_access_logs_enabled] = self.enable_access_logs_val
+  if not self.custom_logging then
+    return
+  end
+
+  -- Extended log is now enaled, disable the default access_log
+  ngx.var[ngx_var_access_logs_enabled] = 0
+  
+  local extended_context = get_request_context(context or {})
+  if self.condition and not self.condition:evaluate(extended_context) then
+    -- Access log is disabled here, request does not match, so log is disabled
+    -- for this request
+    ngx.var[ngx_var_extended_access_logs_enabled] = 0
+    return
+  end
+
+  ngx.var[ngx_var_extended_access_logs_enabled] = 1
+  local tmpl = TemplateString.new(self.custom_logging, "liquid")
+  ngx.var[ngx_var_extended_access_log] = tmpl:render(extended_context)
 end
 
 return _M
--- spec/policy/logging/logging_spec.lua
+++ spec/policy/logging/logging_spec.lua
@@ -1,4 +1,5 @@
 local LoggingPolicy = require('apicast.policy.logging')
+local ngx_variable = require('apicast.policy.ngx_variable')
 
 describe('Logging policy', function()
   describe('.log', function()
@@ -35,5 +36,83 @@ describe('Logging policy', function()
         assert.equals('1', ngx.var.access_logs_enabled)
       end)
     end)
+  end)
+
+  describe("Extened log", function()
+    local ctx = { service = {id=123} }
+    before_each(function()
+      ngx.var = {foo = "fooValue"}
+      stub(ngx.req, 'get_headers', function() return { } end)
+      stub(ngx.resp, 'get_headers', function() return { } end)
+      stub(ngx_variable, 'available_context', function(ctx) return ctx end)
+    end)
+
+    it("Default access log is disabled when is defined", function()
+      local logging = LoggingPolicy.new({custom_logging="foo"})
+      logging:log(ctx)
+      assert.equals(0, ngx.var.access_logs_enabled)
+      assert.equals(1, ngx.var.extended_access_logs_enabled)
+      assert.equals("foo", ngx.var.extended_access_log)
+    end)
+
+    it("log message render information from context and ngx.var", function()
+
+      local logging = LoggingPolicy.new({
+        custom_logging=">>{{foo}}::{{service.id}}"
+      })
+      logging:log(ctx)
+      assert.equals(0, ngx.var.access_logs_enabled)
+      assert.equals(1, ngx.var.extended_access_logs_enabled)
+      assert.equals(">>fooValue::123", ngx.var.extended_access_log)
+    end)
+
+    describe("Conditions", function()    
+      
+      it("Conditions only log if matches", function()
+        local logging = LoggingPolicy.new({
+          custom_logging = "foo",
+          condition = {
+            operations={{op="==", match="{{ foo }}", match_type="liquid", value="fooValue", value_type="plain"}},
+            combine_op="and"
+          }})
+        logging:log(ctx)
+        assert.equals(0, ngx.var.access_logs_enabled)
+        assert.equals(1, ngx.var.extended_access_logs_enabled)
+        assert.equals("foo", ngx.var.extended_access_log)
+      end)
+
+
+      it("Or combination match one", function()
+        local logging = LoggingPolicy.new({
+          custom_logging = "foo",
+          condition = {
+            operations={
+              {op="==", match="{{ invalid }}", match_type="liquid", value="fooValue", value_type="plain"},
+              {op="==", match="{{ foo }}", match_type="liquid", value="fooValue", value_type="plain"}
+            },
+            combine_op="or"
+          }})
+        logging:log(ctx)
+        assert.equals(0, ngx.var.access_logs_enabled)
+        assert.equals(1, ngx.var.extended_access_logs_enabled)
+        assert.equals("foo", ngx.var.extended_access_log)
+      end) 
+
+      it("No Match combination", function()
+        local logging = LoggingPolicy.new({
+          custom_logging = "foo",
+          condition = {
+            operations={{op="==", match="{{ invalid }}", match_type="liquid", value="fooValue", value_type="plain"}},
+            combine_op="and"
+          }})
+        logging:log(ctx)
+        assert.equals(0, ngx.var.access_logs_enabled)
+        assert.equals(0, ngx.var.extended_access_logs_enabled)
+        assert.is_nil(ngx.var.extended_access_log)
+      end)
+    end)
+
+
+
   end)
 end)
